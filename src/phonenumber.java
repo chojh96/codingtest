6f6f1f1a8819ef1cd865ba85eba0fa387387656b
jh <a01031633466@gmail.com>
2022-06-04 20:50:43 +0900

    1

---
+++ src/phonenumber.java
@@ -0,0 +1,15 @@
+public class phonenumber {
+    public String solution(String phone_number) {
+        String answer = "";
+
+        for(int i = 0; i < phone_number.length(); i ++) {
+            if(i < phone_number.length() - 4){
+                answer += "*";
+            }
+            else{
+                answer += phone_number.charAt(i);
+            }
+        }
+        return answer;
+    }
+}
