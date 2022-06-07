0e8e044ae5abd585536d7a21d75746b0c275190a
jh <a01031633466@gmail.com>
2022-06-07 19:17:02 +0900

    week2_evenAndOdd

---
+++ src/evenAndOdd.java
@@ -0,0 +1,12 @@
+public class evenAndOdd {
+    public String solution(int num) {
+        String answer = "";
+
+        if(num % 2 == 0)
+            answer = "Even";
+        else
+            answer = "Odd";
+
+        return answer;
+    }
+}
